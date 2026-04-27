c34caf5d623a8fc707d54007eec36d0fdf503066
JurMich2123 <michlik.juraj2123@gmail.com>
2026-04-27 21:28:31 +0200

    Refactor database connection variables for clarity

--- db.php
+++ db.php
@@ -1,6 +1,14 @@
 <?php
-$pripojenie = mysqli_connect("localhost", "root", "root", "gametracker");
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$db   = "gametracker";
+
+$pripojenie = mysqli_connect($host, $user, $pass, $db);
 if (!$pripojenie) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+$conn = $pripojenie;
+$pripojenie->set_charset("utf8mb4");
 ?>
